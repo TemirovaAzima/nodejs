5d123b26a73cce4048badb91e2275bf77a15f748
TemirovaAzima <temirovaazima9@gmail.com>
2025-05-08 01:55:17 +0500

    readdir() end

--- FS Module/promiseApi/index.js
+++ FS Module/promiseApi/index.js
@@ -1,15 +1,24 @@
 import * as fs from "fs/promises"
 // const fs  = require('node:fs').promises
-try{
-    await fs.writeFile("c:\\nodejs\\home\\text.txt",'hello man',"utf-8")
-    console.log('nodejs file created')
-}catch(err){
-    console.log(err)
-}
+// try{
+//     await fs.writeFile("c:\\nodejs\\home\\text.txt",'hello man',"utf-8")
+//     console.log('nodejs file created')
+// }catch(err){
+//     console.log(err)
+// }
 // recursive true is good for deleting everything like subfolders , nested files if u dont use it u will get
 // error like the dir is not empty
 // force true when u try to delete like it twice the second it will give an error like there is no such file exist
 // so to ignore that u have force true
-
-
 // todo delete the non-empty folder on command prompt u can use rmdir /s /q home
+
+
+try{
+    const files = await fs.readdir('c:\\Users\\azima\\OneDrive',"utf-8");
+    for(const file of files){
+        console.log(file)
+    }
+    console.log(files)
+}catch(err){
+    console.log(err)
+}
