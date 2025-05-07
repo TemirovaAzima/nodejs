a35e384d7c65efd78bf2e564697299c5b8c861aa
TemirovaAzima <temirovaazima9@gmail.com>
2025-05-08 01:13:34 +0500

    rm() removing directories with force true end

--- FS Module/promiseApi/index.js
+++ FS Module/promiseApi/index.js
@@ -1,8 +1,12 @@
 import * as fs from "fs/promises"
 // const fs  = require('node:fs').promises
 try{
-    await fs.mkdir("c:\\nodejs\\home",{recursive:true})
-    console.log('nodejs folder created')
+    await fs.rm("c:\\nodejs\\home",{recursive:true,force:true})
+    console.log('nodejs folder deleted ')
 }catch(err){
     console.log(err)
 }
+// recursive true is good for deleting everything like subfolders , nested files if u dont use it u will get
+// error like the dir is not empty
+// force true when u try to delete like it twice the second it will give an error like there is no such file exist
+// so to ignore that u have force true
