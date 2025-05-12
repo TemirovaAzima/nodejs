5aed708f600bd5ab7828fcc2531d645ae9486586
TemirovaAzima <temirovaazima9@gmail.com>
2025-05-12 23:16:04 +0500

    readFile() and writeFile() with flag property end in callbackApi

--- FS Module/callbackApi/index.js
+++ FS Module/callbackApi/index.js
@@ -1,6 +1,19 @@
 import * as fs from 'fs';
 
-fs.mkdir('c:\\nodejs\\home',(err)=>{
-    if(err) throw err
-    console.log('node js folder created')
+// fs.mkdir('c:\\nodejs\\home',(err)=>{
+//     if(err) throw err
+//     console.log('node js folder created')
+// })
+
+// fs.writeFile('c:\\nodejs\\home\\image.text', 'it is a picture','utf8',(err)=>{
+//     if(err) throw err;
+//     console.log('Successfully written');
+// })
+// fs.readFile('c:\\nodejs\\home\\image.text','utf8',(err,data)=>{
+//     if(err) throw err
+//     console.log(data)
+// })
+fs.writeFile('c:\\nodejs\\home\\image.text','using flag property',{encoding:'utf8',flag:'a'}, (err)=>{
+   if(err) throw err
+    console.log('Used flag property')
 })
