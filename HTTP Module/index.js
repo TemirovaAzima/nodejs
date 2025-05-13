ba80102690adec3b73df18b48798e3b7bfb06b76
TemirovaAzima <temirovaazima9@gmail.com>
2025-05-13 16:28:33 +0500

    setHeader() with json

--- HTTP Module/index.js
+++ HTTP Module/index.js
@@ -5,9 +5,11 @@ const server = http.createServer((req,res)=>{
     // res.statusMessage = 'Not Found'
     // res.write("<h1>hello </h1>")
     // res.end('Bye')
-    res.writeHead(202,"Good",{"Content-Type": 'text/html'})
-    res.write('Hello world')
-    res.end('hey again')
+    // res.writeHead(202,"Good",{"Content-Type": 'text/html'})
+    // res.write('Hello world')
+    // res.end('hey again')
+    res.setHeader("Content-Type","application/json")
+    res.end(JSON.stringify({name:"Azima",age:16}))
 })
 
 server.listen(8000,()=> console.log('Server running on port 8000'))
