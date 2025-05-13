fcbb409b402120d6a13b3406307c384858466485
TemirovaAzima <temirovaazima9@gmail.com>
2025-05-13 16:12:16 +0500

    using writeHead() instead of statusCode and statusMessage

--- HTTP Module/index.js
+++ HTTP Module/index.js
@@ -1,10 +1,13 @@
 import * as http from 'http';
 
 const server = http.createServer((req,res)=>{
-    res.statusCode = 404
-    res.statusMessage = 'Not Found'
-    res.write("<h1>hello </h1>")
-    res.end('Bye')
+    // res.statusCode = 404
+    // res.statusMessage = 'Not Found'
+    // res.write("<h1>hello </h1>")
+    // res.end('Bye')
+    res.writeHead(202,"Good",{"Content-Type": 'text/html'})
+    res.write('Hello world')
+    res.end('hey again')
 })
 
 server.listen(8000,()=> console.log('Server running on port 8000'))
