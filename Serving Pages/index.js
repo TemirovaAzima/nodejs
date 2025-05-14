124bab2460590cd19e6dfa3fd3a8f35c62620b91
TemirovaAzima <temirovaazima9@gmail.com>
2025-05-15 00:40:16 +0500

    different example with serving pages end

--- Serving Pages/index.js
+++ Serving Pages/index.js
@@ -3,23 +3,21 @@ import http from 'http';
 
 const server = http.createServer((req,res)=>{
     if(req.url === '/'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
         fs.readFile('../public/home.html',(err,data)=>{
-           if(err) throw err
-            res.end(data)
-        })
+            if(err){
+                res.writeHead(500);
+                res.end('Error loading page');
+            }else{
+                res.writeHead(200,{'Content-Type': 'text/html'});
+                res.end(data)
+            }
+        });
+    }else{
+        res.writeHead(404);
+        res.end('Page not found')
     }
-    else if (req.url === '/about'){
-        res.writeHead(200,{'Content-Type':'text/html'});
-        fs.readFile('../public/about.html','utf8', (err,data)=>{
-            if(err) throw err
-            res.end(data)
-        })
-    }
-    else{
-        res.writeHead(404,{'Content-Type':'text/html'});
-        res.end('<h1>404 Not Found</h1>')
-    }
-})
+});
 
-server.listen(8000,()=> console.log('Server started'))
+server.listen(8000,()=>{
+    console.log('Server running on port 3000');
+})
