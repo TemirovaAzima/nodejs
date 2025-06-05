e347b7a1454b0e43d05b874766ce9dc3f13f9ad1
TemirovaAzima <temirovaazima9@gmail.com>
2025-06-05 22:42:45 +0500

    null and undefined checking

--- BTS/index.js
+++ BTS/index.js
@@ -1,3 +1,12 @@
-console.log("Program end");
-alert('Block');
-console.log("Program end")
+// console.log("Start");
+// setTimeout(()=>{
+//     alert("non-blocking code")
+// },0);
+//
+// console.log("End")
+// console.log(name)
+// let name = 'azima'
+
+// console.log(clientAge) // is not defined === null
+let clientAge
+console.log(clientAge) // undefined
