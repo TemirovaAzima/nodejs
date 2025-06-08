1e059f1f3fc99512d9441d513cc8f4b8fc0d8539
TemirovaAzima <temirovaazima9@gmail.com>
2025-06-08 14:21:29 +0500

    setImmediate inside on() listener

--- Event/index.js
+++ Event/index.js
@@ -1,11 +1,20 @@
-const EventEmitter = require('node:events');
+import {EventEmitter} from 'node:events';
 
-class MyEmitter extends EventEmitter {}
-// extending allows u to create own custom object of EventEmitter , meaning that u can add own properties and methods apart from that is built in
-// you are creating ur own custom version of EventEmitter , allowing to add custom methods or properties
-// difference between class and object is that class is a blueprint like human and object is a actual instance if the blueprint like Richael person
-const myEmitter = new MyEmitter();
-myEmitter.on('event',()=>{
-    console.log('an event occurred!');
-});
-myEmitter.emit('event');
+class MyEmitter extends EventEmitter {
+}
+
+console.log(EventEmitter)
+const myEmitter = new MyEmitter()
+
+myEmitter.on('event', () => {
+    console.log('Not asynchronous')
+})
+myEmitter.on('event', (a, b) => {
+    setImmediate(() => {
+        console.log(`this happen asynchronously ${a},${b}`)
+    })
+    console.log("checking")
+})
+console.log('start')
+myEmitter.emit('event', 'a', 'b')
+console.log('end')
