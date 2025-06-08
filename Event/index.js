9bf959be2b357b66dfb114f0c5d573537354eb7f
TemirovaAzima <temirovaazima9@gmail.com>
2025-06-08 16:06:17 +0500

    captureRejections end

--- Event/index.js
+++ Event/index.js
@@ -1,16 +1,17 @@
-import {EventEmitter, errorMonitor} from 'events';
-import * as util from "node:util";
+import events from 'node:events';
+events.captureRejections = true;
 
-class MyEventEmitter extends EventEmitter {}
-const myEmitter = new MyEventEmitter();
-// errorMonitor just observes and doesn't prevent crashing
-
-myEmitter.on(errorMonitor,(err)=>{
-    console.log(util.inspect(err),'there was an error')
+// u always should handle promise rejection because it will crash ur Nodejs
+// u can either use captureRejections or handle properly the rejection with .on('error')
+// or use both to handle everything wonderfully
+const emitter = new events.EventEmitter();
+emitter.on('myEvent',async()=>{
+    throw new Error('Async error')
 });
-myEmitter.on('error',(err)=>{
-    console.log(err,'hello')
+console.log('middle')
+emitter.on('error',(err)=>{
+    console.log('Caught:',err.message)
 })
-myEmitter.emit('error',new Error("whoops!"));
+
+emitter.emit('myEvent')
 console.log('end')
-//
