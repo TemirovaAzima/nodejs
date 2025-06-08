ab43183fafcd04511de3b0cca0b120b002be24c5
TemirovaAzima <temirovaazima9@gmail.com>
2025-06-08 14:52:00 +0500

    managing error by preventing from crashing Nodejs

--- Event/index.js
+++ Event/index.js
@@ -1,7 +1,12 @@
 import {EventEmitter} from 'events';
-class MyEventEmitter extends EventEmitter {}
 
+class MyEventEmitter extends EventEmitter {}
 const myEmitter = new MyEventEmitter();
 
+// error is built name type so make sure u don't misspell it
+
+myEmitter.on('error',(err)=>{
+    console.error(err,'there was an error')
+});
 myEmitter.emit('error',new Error("whoops!"));
-// throws and crashes Nodejs
+// u are managing error if u dont nodejs crashes
