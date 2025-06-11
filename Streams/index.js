673f21d7e43fdd672695e778ad8cbf041ef51229
TemirovaAzima <temirovaazima9@gmail.com>
2025-06-11 16:21:48 +0500

    Prove:streams is for big data

--- Streams/index.js
+++ Streams/index.js
@@ -1,6 +1,16 @@
 import fs from 'fs';
 
-const rs = fs.createReadStream('c:\\nodejs\\text\\text.text');
-rs.on('open',function(){
-    console.log('This file is open')
-})
+// For small data, doing this is faster and easier.
+const content = fs.readFileSync('c:\\nodejs\\text\\text.text','utf8');
+
+console.log(content.toString());
+// But for big files(like 1GB+), that could crash your server. That's where streams are much safer
+const stream = fs.createReadStream('c:\\nodejs\\text\\text.text','utf8');
+
+stream.on('data',(chunk)=>{
+    console.log('Received chunk',chunk.toString());
+});
+
+stream.on('end',()=>{
+    console.log('Finished reading');
+});
