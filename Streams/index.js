0e14c015ee3d8d2f193bf41d0015ad9b2407d6e1
TemirovaAzima <temirovaazima9@gmail.com>
2025-06-12 16:11:03 +0500

    Streams end

--- Streams/index.js
+++ Streams/index.js
@@ -1,21 +1,3 @@
-import fs from 'fs';
-
-// Examples of Creating/ Adding / Managing Data with Streams
-// reading
-const reader = fs.createReadStream('c:\\nodejs\\text\\text.text');
-reader.on('data',(chunk)=>{
-    console.log('Read:',chunk);
-});
-
-
-// writing
-const writer = fs.createWriteStream('c:\\nodejs\\text\\text.text');
-writer.write('This is a new line\n');
-writer.end();
-console.log(writer)
-
-// piping(managing)
-const reading = fs.createReadStream('c:\\nodejs\\text\\text.text');
-const writing = fs.createWriteStream('c:\\nodejs\\text\\newText.text');
-reading.pipe(writing) // read and write in one flow
-console.log(writing)
+import {createReadStream} from 'fs'
+const stream = createReadStream('./data.txt',{encoding:'utf8'});
+stream.on('data',(data)=>console.log(data));
